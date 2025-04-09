89a9be44a3f8d06014e4d6188d0ebe955abb60c8
Trump <128599027+LetAmericaGreatAgain@users.noreply.github.com>
2025-04-09 10:24:54 +0800

    first commit

--- IceModel.h
+++ IceModel.h
@@ -19,19 +19,19 @@ private:
     bool _h;
 
 public:
-    /* 构造函数改为手动配置 */
+    
     IceModel()
     {
-        _n = false;  // 不使用 STUN
-        _m = false;  // 不使用 UDP multiplex
-        _s = "stun.l.google.com"; // STUN 服务器地址
-        _t = 19302;   // STUN 端口
-        _w = "localhost"; // WebSocket 服务器
-        _x = 8000;    // WebSocket 端口
-        _h = false;   // 是否显示帮助信息
+        _n = false; 
+        _m = false;  
+        _s = "stun.l.google.com"; 
+        _t = 19302;   
+        _w = "localhost";
+        _x = 8000;    
+        _h = false;   
     }
 
-    /* 手动设置参数的方法 */
+    
     void setNoStun(bool n) { _n = n; }
     void setUdpMux(bool m) { _m = m; }
     void setStunServer(const std::string& s) { _s = s; }
@@ -39,7 +39,7 @@ public:
     void setWebSocketServer(const std::string& w) { _w = w; }
     void setWebSocketPort(int x) { _x = x; }
 
-    /* 读取参数 */
+    
     bool noStun() const { return _n; }
     bool udpMux() const { return _m; }
     std::string stunServer() const { return _s; }
